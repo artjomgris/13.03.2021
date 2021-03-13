6d6f21f62650837f3a48581f1e171331b065cd39
artjomgris <47395399+artjomgris@users.noreply.github.com>
2021-03-13 12:45:41 +0200

    V1.0

---
+++ client.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+)
+
+type Action struct {
+	Act string `json:"action"`
+	Id  int    `json:"id"`
+}
+
+type Update struct {
+	Act  Action
+	Text string `json:"body"`
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:5000")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	var data []byte
+	for {
+		var inp Action
+		in := bufio.NewReader(os.Stdin)
+		fmt.Println("Enter Action and Id")
+		fmt.Fscanf(in, "%v %v\n", &inp.Act, &inp.Id)
+		if inp.Act == "update" || inp.Act == "Update" {
+			var upd Update
+			upd.Act = inp
+			fmt.Println("Enter Text (Use _ instead of spaces)")
+			fmt.Scan(&upd.Text)
+			form, err := json.Marshal(upd)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			//fmt.Println(string(form))
+			data = append(data, form...)
+		} else {
+			form, err := json.Marshal(inp)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			//fmt.Println(string(form))
+			data = append(data, form...)
+		}
+		fmt.Println("Enter stop to stop or Press \"enter\" to continue")
+		var cont string
+		fmt.Fscanf(in, "%v\n", &cont)
+		if cont == "stop" {
+			break
+		}
+		data = append(data, byte(','))
+	}
+
+	conn.Write(data)
+
+	buf := make([]byte, 2000)
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(buf[:n]))
+}
---
+++ server.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"strings"
+)
+
+type Action struct {
+	Act string `json:"action"`
+	Id  int    `json:"id"`
+}
+
+type Update struct {
+	Act  Action
+	Text string `json:"body"`
+}
+
+var DB = []string{"Hello Wordl!", "Text", "Lorem Ipsum", "Posts", "Rolling Stone", "Windows"}
+
+func main() {
+	l, err := net.Listen("tcp", "127.0.0.1:5000")
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go HandleConnection(conn)
+	}
+}
+
+func HandleConnection(conn net.Conn) {
+	buf := make([]byte, 2000)
+	n, err := conn.Read(buf)
+	if err != nil {
+		conn.Close()
+		return
+	}
+	var action Action
+	data := string(buf[:n])
+	for {
+		i := strings.Index(data, "},{")
+		if i > 4 {
+			err := json.Unmarshal([]byte(data[:i+1]), &action)
+			if err != nil {
+				panic(err)
+			}
+			if len(action.Act) < 4 {
+				fmt.Println("update")
+			}
+
+			fmt.Println(action)
+			data = data[i+2:]
+			i = 0
+		} else {
+			break
+		}
+	}
+	err = json.Unmarshal([]byte(data), &action)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(action)
+
+	conn.Write([]byte("Connection great"))
+
+	conn.Close()
+}
